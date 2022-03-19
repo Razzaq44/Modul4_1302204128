058f39ad398db25fe7af40387b212c3d63eff33b
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-19 10:00:48 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Modul4_1302204128/Program.cs
+++ Modul4_1302204128/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace mod4
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Penjumlahan.JumlahTigaAngka<int>(13, 02, 20);
+        }
+    }
+
+
+    class Penjumlahan
+    {
+        public static void JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+        {
+            dynamic tempA = angka1;
+            dynamic tempB = angka2;
+            dynamic tempC = angka3;
+
+            Console.WriteLine(tempA + tempB + tempC);
+        }
+    }
+}
+    
